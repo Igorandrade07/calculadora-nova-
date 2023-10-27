1054c62a5e75220b42ac0e9101ce6405c6d06ad1
Igorandrade07 <136855017+Igorandrade07@users.noreply.github.com>
2023-10-27 17:12:52 -0300

    Update Calculos.js

--- Calculos.js
+++ Calculos.js
@@ -1,5 +1,5 @@
 function multiplica() {
     num1 = document.getElementById("n1").value;
     num2 = document.getElementById("n2").value;
-    document.getElementById("resultado").innerHTML = num1 * num2;
+    document.getElementById("resultado").innerHTML = num1 ** num2;
 }
