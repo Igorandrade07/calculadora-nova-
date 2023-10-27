fde1714e56922b7edb1dd4b0ff671ef25ed23715
Igorandrade07 <136855017+Igorandrade07@users.noreply.github.com>
2023-10-27 16:58:07 -0300

    Update Calculos.js

--- Calculos.js
+++ Calculos.js
@@ -3,9 +3,3 @@ function multiplica() {
     num2 = document.getElementById("n2").value;
     document.getElementById("resultado").innerHTML = num1 * num2;
 }
-
-function divide() {
-    num1 = document.getElementById("n1").value;
-    num2 = document.getElementById("n2").value;
-    document.getElementById("resultado").innerHTML = num1 / num2;
-}
